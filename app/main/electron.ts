7ac99e9e77150058468a763f13f5716d4c5a3a7f
彭士兴 <2644378911@qq.com>
2024-06-12 22:51:47 +0800

    feat: 添加ipc通信

--- app/main/electron.ts
+++ app/main/electron.ts
@@ -2,7 +2,7 @@
  * @desc electron 主入口
  */
 import path from 'path';
-import { app, BrowserWindow, globalShortcut } from 'electron';
+import { app, BrowserWindow, globalShortcut, ipcMain } from 'electron';
 
 function isDev() {
   // 👉 还记得我们配置中通过 webpack.DefinePlugin 定义的构建变量吗
@@ -59,4 +59,10 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+});
+
+const ROOT_PATH = path.join(app.getAppPath(), '../');
+
+ipcMain.on('get-root-path', (event, arg) => {
+  event.reply('reply-root-path', ROOT_PATH);
 });
