95bee31f6eb937b9071387e280ea3daa904de6ab
彭士兴 <2644378911@qq.com>
2024-06-15 00:37:12 +0800

    fix: 简历页面划分

---
+++ app/renderer/components/ResumeAction/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.less';
+
+function ResumeAction() {
+  return <div>我是简历头部操作区</div>;
+}
+export default ResumeAction;
--- app/renderer/container/Resume/index.tsx
+++ app/renderer/container/Resume/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import ResumeAction from '../../components/ResumeAction';
+import ResumeContent from '../../components/ResumeContent';
+import ResumeToolbar from '../../components/ResumeToolbar';
 import './index.less';
-import fileAction from '@common/utils/file';
-import { getAppPath } from '@common/utils/appPath';
 
 function Resume() {
-  getAppPath().then((rootPath: string) => {
-    console.log('应用程序的目录路径为: ', rootPath);
-    console.log('文件读取，内容数据为: ');
-    fileAction.write(`${rootPath}app/renderer/container/resume/test.tsx`, '1213').then((data) => {
-      console.log(data);
-    });
-  });
-  return <div>我是简历模块</div>;
+  return (
+    <div styleName="container">
+      <div styleName="header">
+        <ResumeAction />
+      </div>
+      <div styleName="content">
+        <ResumeContent />
+      </div>
+      <div styleName="toolbar">
+        <ResumeToolbar />
+      </div>
+    </div>
+  );
 }
 export default Resume;
